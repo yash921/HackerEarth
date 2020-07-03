45c6de503d79ab4c90d99488fa7759457f401332
yash dixit <yashdixit921@gmail.com>
2020-07-03 17:36:46 +0530

    Palindromic-String

---
+++ Palindromic-String.py
@@ -0,0 +1,33 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+s = input()
+def isPalindrome(s):
+
+    def toChars(s):
+        s = s.lower()
+        ans = ''
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                ans = ans + c
+        return ans
+
+    def isPal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and isPal(s[1:-1])
+    def toCheck(s):
+        if s == True:
+            print("YES")
+        else:
+            print("NO")
+    return toCheck(isPal(toChars(s)))
+isPalindrome(s)
