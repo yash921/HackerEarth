6abe877c5d6263d307f83eac8ae5a3ebad681a8c
yash dixit <yashdixit921@gmail.com>
2020-07-03 18:07:19 +0530

    Palindromic-String using recursion

--- Palindromic-String.py
+++ Palindromic-String.py
@@ -1,12 +1,3 @@
-'''
-# Sample code to perform I/O:
-
-name = input()                  # Reading input from STDIN
-print('Hi, %s.' % name)         # Writing output to STDOUT
-
-# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-'''
-
 # Write your code here
 s = input()
 def isPalindrome(s):
