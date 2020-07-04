6e9221b2e1d6e4caff88d7f1415d1770dc10ff45
yash dixit <yashdixit921@gmail.com>
2020-07-04 18:18:18 +0530

    Toggle string v1/v2

---
+++ Toggle-String.py
@@ -0,0 +1,27 @@
+result='' 
+txt=input() 
+for i in txt: 
+   if i.islower(): 
+       result+=i.upper() 
+   else: 
+       result+=i.lower() 
+print(result)
+
+
+#################################################
+
+#s = input()
+#l = []
+#string = ""
+#for i in s:
+#    if i.islower():
+#        z = i.upper()
+#        l.append(z)
+#    else:
+#        x = i.lower()
+#        l.append(x)
+#print(string.join(l))
+
+
+
+
